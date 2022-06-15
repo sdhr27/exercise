3f442f7b5af1c6d0741fdd76db3fffd3236935bb
段佳骊 <duan.jiali@datatom.com>
2022-06-15 14:16:48 +0800

    chore: 调整eslint规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,23 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
     es2021: true,
+    jest: true,
   },
-  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 'latest',
+    sourceType: 'module',
   },
+  plugins: ['react', '@typescript-eslint'],
   rules: {
     'no-use-before-define': 'off',
     'consistent-return': 'off',
