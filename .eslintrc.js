712166f9a802abf24ad7fe7a902149a60170102d
段佳骊 <duan.jiali@datatom.com>
2022-06-15 16:12:48 +0800

    chore: 调整eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
   env: {
     browser: true,
+    commonjs: true,
     es2021: true,
     jest: true,
   },
   extends: [
-    'eslint:recommended',
+    'airbnb-base',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
   ],
@@ -17,12 +18,29 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.ts'],
+      },
+    },
+    'import/extensions': ['.js', '.jsx', '.tsx', '.ts'],
+  },
   rules: {
     'no-use-before-define': 'off',
     'consistent-return': 'off',
     'new-cap': 'off',
     'no-plusplus': 'off',
     'no-bitwise': 'off',
+    quotes: 'off',
+    'import/extensions': 'off',
+    'one-var': 'off',
+    'implicit-arrow-linebreak': 'off',
+    'no-confusing-arrow': 'off',
+    'function-paren-newline': 'off',
   },
 };
